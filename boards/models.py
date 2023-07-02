65841624eedc372eb124888e6ec53a4bd45e5b8a
thovy <98632452+thovy@users.noreply.github.com>
2023-07-02 21:20:05 +0900

    feat: create project, apps, models
    1. tosstock 프로젝트 생성
    2. accounts app 생성
    boards app 생성
    3. allauth app 을 비롯한 app 들 settings 에 등록(update settings)
    - allauth setting 을 완전히 하지는 않음. 추후에 user(accounts)가 완성되면 업데이트 예정
    4. boards app 에 article 모델, comment 모델 생성

---
+++ boards/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+from django.conf import settings
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    views = models.IntegerField()
+
+    # user 가 탈퇴해도 게시글은 남아있도록하려면 CASCADE만 빼면 되나?
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='articles', on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='comments', on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, related_name='comments', on_delete=models.CASCADE)
