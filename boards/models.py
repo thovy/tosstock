1d00c8e6894942b980ab4e1702263bd70415a4e7
thovy <98632452+thovy@users.noreply.github.com>
2023-07-04 17:42:02 +0900

    refactor: user 와 article, comment on_delete 관계 수정
    user 를 삭제해도 article 과 comment 내용이 남아있도록 on_delete 관계를 수정했습니다. set Null 을 이용했는데 올바른 수정은 아닙니다. 추후에 Set Default 나 Set 을 이용하는 방향으로 다시 수정할 생각입니다.

--- boards/models.py
+++ boards/models.py
@@ -9,8 +9,7 @@ class Article(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     views = models.IntegerField(default=0)
 
-    # user 가 탈퇴해도 게시글은 남아있도록하려면 CASCADE만 빼면 되나?
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='articles', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='articles', on_delete=models.SET_NULL, null=True)
 
     helpful_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='helpful_articles')
     unhelpful_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='unhelpful_articles')
@@ -23,7 +22,12 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='comments', on_delete=models.CASCADE)
+    # user 가 삭제되어도 comment 가 삭제되지 않도록
+    # SET_NULL 을 이용해 user 가 삭제되면 null 을 넣도록했는데,
+    # user 가 null 이어도 문제가 없나?
+    # - null 이면 문제가 생기기 때문에 Set Default 혹은 Set 을 이용하도록 합시다.
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='comments', on_delete=models.SET_NULL, null=True)
+    # article이 삭제되면 comment도 삭제되도록 cascade
     article = models.ForeignKey(Article, related_name='comments', on_delete=models.CASCADE)
 
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_comments')
