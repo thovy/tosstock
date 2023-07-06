1658ff1b3cabca9e0ba408ce0d369675bd0bc630
thovy <98632452+thovy@users.noreply.github.com>
2023-07-06 09:47:47 +0900

    refactor: user detail 기능 수정
    1. user 상세보기 path 수정
    2. user 상세보기 내 comment serializer 추가로 user의 comment 상세내역 출력 기능 추가

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from boards.models import Article
+from boards.models import Article, Comment
 
 class UserSerializer(serializers.ModelSerializer):
 
@@ -9,10 +9,17 @@ class UserSerializer(serializers.ModelSerializer):
         class Meta:
             model = Article
             fields = ('pk', 'title', 'content', 'views')
+
+    class CommentSerializer(serializers.ModelSerializer):
+
+        class Meta:
+            model = Comment
+            fields = ('pk', 'content', )
     
     helpful_articles = ArticleSerializer(many=True)
     unhelpful_articles = ArticleSerializer(many=True)
     articles = ArticleSerializer(many=True)
+    comments = CommentSerializer(many=True)
 
     class Meta:
         model = get_user_model()
