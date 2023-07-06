a1993163e0f5ed84abfd2c94786e48cc29f72ef5
thovy <98632452+thovy@users.noreply.github.com>
2023-07-06 14:59:12 +0900

    refair: url path 오타 수정
    urls path 오타 수정

--- news/urls.py
+++ news/urls.py
@@ -5,7 +5,7 @@ app_name = 'news'
 
 urlpatterns = [
     path('', views.news_all),
-    path('/subject/<str:subject>/', views.field_news),
+    path('subject/<str:subject>/', views.field_news),
     path('<int:news_pk>/', views.news_detail),
     path('<int:news_pk>/helpful/', views.helpful_news),
     path('<int:news_pk>/unhelpful/', views.unhelpful_news),
