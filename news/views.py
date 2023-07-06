ef070e29b057687f16cc7d742adb4d1d180fe1b6
thovy <98632452+thovy@users.noreply.github.com>
2023-07-06 13:08:47 +0900

    feat: news 전체 조회 기능 구현
    1. news 전체 조회 기능 구현

--- news/views.py
+++ news/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404
+from django.db.models import Count
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+
+from .models import Field, News
+from .serializers import FieldSerializer, NewsListSerializer, NewsSerializer
 
 # 뉴스 전체보기
+@api_view(['GET'])
 def news_all(request):
-    pass
+    all_news = News.objects.annotate(
+        helpful_count = Count('helpful_users', distinct=True),
+        unhelpful_count = Count('unhelpful_users', distinct=True),
+    ).order_by('-pk')
+
+    serializer = NewsListSerializer(all_news, many=True)
+    return Response(serializer.data)
 
 # 뉴스 생성 - user 가 글을 쓰는 게 아닌, 크롤링으로 등록
 def create_news():
