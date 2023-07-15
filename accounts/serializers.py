8050245990b4d66fd915ea514c430e1c8fc23158
thovy <98632452+thovy@users.noreply.github.com>
2023-07-15 21:44:49 +0900

    feat: 빠져있던 bookmark news 추가
    1. user serializer 에 빠져있던 bookmark news 를 추가했습니다.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from boards.models import Article, Comment
+from news.models import News
 
 class UserSerializer(serializers.ModelSerializer):
 
@@ -16,11 +17,18 @@ class UserSerializer(serializers.ModelSerializer):
             model = Comment
             fields = ('pk', 'content', )
     
+    class NewsSerializer(serializers.ModelSerializer):
+
+        class Meta:
+            model = News
+            fields = ('pk', 'title', 'isflash', )
+    
     helpful_articles = ArticleSerializer(many=True)
     unhelpful_articles = ArticleSerializer(many=True)
     articles = ArticleSerializer(many=True)
     comments = CommentSerializer(many=True)
+    bookmark_news = NewsSerializer(many=True)
 
     class Meta:
         model = get_user_model()
-        fields = ('pk', 'username', 'email', 'isInfluencer', 'helpful_articles', 'unhelpful_articles', 'articles', 'comments', )
+        fields = ('pk', 'username', 'email', 'isInfluencer', 'helpful_articles', 'unhelpful_articles', 'articles', 'comments', 'bookmark_news')
