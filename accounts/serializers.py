510ea585138685b39732be349e08a6cd9913c5de
thovy <98632452+thovy@users.noreply.github.com>
2023-07-18 00:12:53 +0900

    refactor: user serializer 수정
    1. 다양한 데이터를 하나의 요청으로 확인하기 위해 새롭게 정의했던 article, comment, news serializer 를 없애고 바로 import 받아서 사용하도록 했습니다.
    2. 전체를 받아서 사용하니 편합니다.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,26 +2,28 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from boards.models import Article, Comment
 from news.models import News, Stock
+from boards.serializers import ArticleSerializer, CommentSerializer
+from news.serializers import NewsSerializer
 
 class UserSerializer(serializers.ModelSerializer):
 
-    class ArticleSerializer(serializers.ModelSerializer):
+    # class ArticleSerializer(serializers.ModelSerializer):
 
-        class Meta:
-            model = Article
-            fields = ('pk', 'title', 'content', 'views')
+    #     class Meta:
+    #         model = Article
+    #         fields = fields = ('pk', 'field', 'user', 'title', 'content', 'views', 'comments', 'helpful_users', 'unhelpful_users')
 
-    class CommentSerializer(serializers.ModelSerializer):
+    # class CommentSerializer(serializers.ModelSerializer):
 
-        class Meta:
-            model = Comment
-            fields = ('pk', 'content', )
+    #     class Meta:
+    #         model = Comment
+    #         fields = ('pk', 'content', )
     
-    class NewsSerializer(serializers.ModelSerializer):
+    # class NewsSerializer(serializers.ModelSerializer):
 
-        class Meta:
-            model = News
-            fields = ('pk', 'title', 'isflash', )
+    #     class Meta:
+    #         model = News
+    #         fields = ('pk', 'title', 'isflash', )
 
     class StockSerializer(serializers.ModelSerializer):
 
